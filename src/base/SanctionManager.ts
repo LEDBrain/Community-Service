89688192b82a65833ad5f20e5944a90dcef33177
kdev <konstantinw@ledbrain.de>
2022-02-28 21:13:03 +0100

    chore(SanctionManager): add some (weird) logic for sacntion handling

--- src/base/SanctionManager.ts
+++ src/base/SanctionManager.ts
@@ -11,10 +11,6 @@ export default class SanctionManager {
     private db: typeof prisma;
 
     public id: number;
-    public timestamp: Date;
-    public approved_by: string[];
-    public sanctioning_end: Date;
-    public terminatedBy: number;
 
     constructor(
         member: string,
@@ -32,7 +28,7 @@ export default class SanctionManager {
     }
 
     async create() {
-        return await this.db.sanction.create({
+        const newSanction = await this.db.sanction.create({
             data: {
                 type: this.type,
                 reason: this.reason,
@@ -58,24 +54,49 @@ export default class SanctionManager {
                 },
             },
         });
+        this.id = newSanction.id;
+        return newSanction;
     }
 
     async link(
         type: Omit<typeof this.type, 'KICK' | 'WARN' | 'UNMUTE'>,
         terminatingSanction: typeof this
     ) {
-        const initialSanction = await this.db.sanction.findFirst({
-            where: {
-                userId: this.member,
-            },
-            include: {
-                TerminatingSanction: true,
-                User: true,
-            },
-            orderBy: {
-                timestamp: 'desc',
-            },
+        return new Promise((resolve, reject) => {
+            this.db.sanction
+                .findFirst({
+                    where: {
+                        AND: {
+                            userId: this.member,
+                            type: type as SanctionType,
+                        },
+                    },
+                    include: {
+                        TerminatingSanction: true,
+                        User: true,
+                    },
+                    orderBy: {
+                        timestamp: 'desc',
+                    },
+                })
+                .then(initialSanction => {
+                    if (!initialSanction || initialSanction.terminatedBy)
+                        return reject();
+                    this.db.sanction
+                        .update({
+                            where: {
+                                id: initialSanction.id,
+                            },
+                            data: {
+                                TerminatingSanction: {
+                                    connect: {
+                                        id: terminatingSanction.id,
+                                    },
+                                },
+                            },
+                        })
+                        .then(() => resolve(true));
+                });
         });
-        console.log(initialSanction);
     }
 }
