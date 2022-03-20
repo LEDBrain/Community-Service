efebc92f4390fa4e203e3e8cfea153a6826835f9
kdev <konstantinw@ledbrain.de>
2022-03-20 15:38:25 +0100

    feat(commands/ban): add Ban command
    re #79

--- src/commands/Ban.ts
+++ src/commands/Ban.ts
@@ -1,4 +1,11 @@
-import type { CommandInteraction, GuildMember, Guild } from 'discord.js';
+import type {
+    CommandInteraction,
+    GuildMember,
+    Guild,
+    TextChannel,
+} from 'discord.js';
+import { MessageActionRow, MessageButton } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 import type { Config } from '../base/Command';
 import Command from '../base/Command';
 import { SlashCommandBuilder } from '@discordjs/builders';
@@ -76,14 +83,98 @@ export default class Ban extends Command {
                 logChannelId: true,
             },
         });
+
         if (!guildSettings?.logChannelId.length)
             return interaction.reply({
                 content: 'No log channel set',
                 ephemeral: true,
             });
 
+        if (force) {
+            await member.ban({
+                reason: reason,
+                days: days,
+            });
+            await interaction.reply({
+                content: `Banned ${member.toString()}`,
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const banRequests = await this.db.banRequest.findFirst({
+            where: {
+                AND: {
+                    guildId: interaction.guild.id,
+                    userId: member.id,
+                },
+            },
+        });
+        if (banRequests)
+            return interaction.reply(
+                `There is already a ban request for ${member.toString()} (see it here: https://discord.com/channels/${
+                    interaction.guild.id
+                }/${guildSettings.logChannelId}/${banRequests.messageId})`
+            );
+
+        const banEmbed = new MessageEmbed()
+            .setTitle('Ban request')
+            .setColor('#ff0000')
+            .addFields([
+                {
+                    name: 'User to be banned',
+                    value: member.toString(),
+                    inline: true,
+                },
+                {
+                    name: 'Moderator',
+                    value: interaction.member.toString(),
+                    inline: true,
+                },
+                {
+                    name: 'Messages to delete',
+                    value: `Days: ${days}`,
+                    inline: true,
+                },
+            ])
+            .setDescription(`Reason:\n${reason}`);
+        const row = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setCustomId('banRequestApprove')
+                .setLabel('Approve')
+                .setStyle('DANGER')
+                .setEmoji('✅'),
+            new MessageButton()
+                .setCustomId('banRequestReject')
+                .setLabel('Reject')
+                .setStyle('PRIMARY')
+                .setEmoji('❌')
+        );
+
+        const message = await (
+            (await interaction.guild.channels.fetch(
+                guildSettings.logChannelId
+            )) as TextChannel
+        ).send({
+            embeds: [banEmbed],
+            components: [row],
+        });
+
+        await this.db.banRequest.create({
+            data: {
+                guildId: interaction.guild.id,
+                messageId: message.id,
+                userId: member.id,
+                moderatorId: (interaction.member as GuildMember).id,
+                reason,
+                daysToDelete: days,
+            },
+        });
+
         await interaction.reply(
-            `${member.user.tag} has been banned for ${reason} (force: ${force}; days: ${days})`
+            `I've created a ban request for ${member.toString()} (see it here: https://discord.com/channels/${
+                interaction.guildId
+            }/${guildSettings.logChannelId}/${message.id})`
         );
     }
 
