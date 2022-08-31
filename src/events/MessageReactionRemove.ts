b974d7f7e0604fd22662870bc45164198ce48d90
kdev <konstantinw@ledbrain.de>
2022-08-31 14:13:24 +0200

    feat(reactionRole/events): Implement ReactionAdd/remove logic for reaction role

---
+++ src/events/MessageReactionRemove.ts
@@ -0,0 +1,40 @@
+import type { MessageReaction, User } from 'discord.js';
+import type Client from '../base/Client';
+import Event from '../base/Event';
+
+export default class MessageReactionRemove extends Event {
+    constructor() {
+        super({ name: 'messageReactionRemove' });
+    }
+    public async execute(
+        _: Client,
+        messageReaction: MessageReaction,
+        user: User
+    ): Promise<void> {
+        const message = await messageReaction.message.fetch();
+        try {
+            const rrMessage =
+                await this.db.reactionRoleMessage.findFirstOrThrow({
+                    where: {
+                        messageId: message.id,
+                    },
+                    include: {
+                        roleToEmoji: true,
+                    },
+                });
+            if (!rrMessage) throw new Error('No reaction role message found');
+            const reactionEmoji = messageReaction.emoji.identifier;
+            const role = rrMessage.roleToEmoji.find(
+                r => r.emojiId === reactionEmoji
+            );
+            if (!role) throw new Error('No role found for the emoji');
+
+            message.guild?.members.fetch(user.id).then(member => {
+                if (member.roles.resolve(role.roleId))
+                    member.roles.remove(role.roleId);
+            });
+        } catch (error) {
+            return;
+        }
+    }
+}
